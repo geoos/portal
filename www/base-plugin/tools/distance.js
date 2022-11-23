f4f330dcaeb7123119c960ff764a6e8e9e7f17c0
“Herman <hopazo@zonar.cl>
2022-11-23 17:15:36 -0300

    Fix DeleteLayer - Zoom position vs layer

--- www/base-plugin/tools/distance.js
+++ www/base-plugin/tools/distance.js
@@ -78,7 +78,7 @@ class ToolDistance extends GEOOSTool {
     }
     
     async isValid() {
-        //console.log("isValid", this.layerId, this.object);
+        //console.log("isValid", this, this.layerId, this.object);
         if (this.object.type.startsWith("user-object/")) return window.geoos.getUserObject(this.object.code)?true:false;
         /*
         let layer = window.geoos.getLayer(this.layerId);
